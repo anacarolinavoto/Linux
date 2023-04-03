83299338141b55e4869c65a34d07678bb35750f2
Ana Carolina Voto <92889219+anacarolinavoto@users.noreply.github.com>
2023-04-03 07:50:20 -0300

    Add files via upload

---
+++ Linux/bd/linux-test.sql
@@ -0,0 +1,40 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 27-Mar-2023 às 14:22
+-- Versão do servidor: 10.4.17-MariaDB
+-- versão do PHP: 7.3.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `linux-test`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `test1`
+--
+
+CREATE TABLE `test1` (
+  `aa1` int(11) NOT NULL,
+  `aa2` int(11) NOT NULL,
+  `aa3` int(11) NOT NULL,
+  `aa4` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
